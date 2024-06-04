8b2a6ac57e84d8555f2fd3c7e6c13feefe478546
nameisdarshil <djshah2000@gmail.com>
2024-06-04 12:10:34 -0400

    new commit

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -5,25 +5,21 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
 export default function Search({ placeholder }: { placeholder: string }) {
-  // export default function Search() {
   const searchParams = useSearchParams();
-  const pathname = usePathname();
   const { replace } = useRouter();
+  const pathname = usePathname();
   
-  function handleSearch(term: string) {
+  // function handleSearch(term: string) {
     const handleSearch = useDebouncedCallback((term) => {
-    console.log(`Searching... ${term}`);  
-    const params = new URLSearchParams(searchParams);
-    params.set('page', '1');
-    if (term) {
-      params.set('query', term);
-    } else {
-      params.delete('query');
-    }
-    replace(`${pathname}?${params.toString()}`);
-    console.log(term);
-  }, 300);
-  }
+      const params = new URLSearchParams(searchParams);
+      params.set('page', '1');
+      if (term) {
+        params.set('query', term);
+      } else {
+        params.delete('query');
+      }
+      replace(`${pathname}?${params.toString()}`);
+    }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
